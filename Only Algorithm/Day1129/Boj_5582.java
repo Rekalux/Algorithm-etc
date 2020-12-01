c0bb67ec362a31b9d5410f783341d30a260c0a42
Rekalux <shycompany@naver.com>
2020-12-02 00:29:51 +0900

    Day1129_2

--- Only Algorithm/Day1129/Boj_5582.java
+++ Only Algorithm/Day1129/Boj_5582.java
@@ -8,18 +8,21 @@ public static void main(String[] args) {
 	String a = sc.next();
 	String b = sc.next();
 	int result = 0;
+	int dp[][] =new int[a.length()][b.length()];
 	for(int i=0;i<a.length();i++) {
 		for(int j=0;j<b.length();j++) {
-			if(a.charAt(i)==b.charAt(j)) {
-				int k = 0;
-				int length = 0;
-				while(j+k < b.length() && i+k <a.length() && a.charAt(i+k) == b.charAt(j+k)) {
-					k++;
-					length++;
-					if(result < length) {
-						result = length;
-					}
+			if(i==0 || j==0) {
+				if(a.charAt(i)==b.charAt(j)) {
+					dp[i][j] = 1;
 				}
+				else {
+					dp[i][j] = 0;
+				}
+				continue;
+			}
+			dp[i][j] = a.charAt(i)==b.charAt(j) ? (dp[i-1][j-1] + 1) : 0;
+			if(dp[i][j] > result) {
+				result = dp[i][j];
 			}
 		}
 	}
