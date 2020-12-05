1d49e7dbad7ea87af50a4e06484edc4c013ccb66
Rekalux <shycompany@naver.com>
2020-12-05 23:56:17 +0900

    Day1205

---
+++ Only Algorithm/Day1205/Boj_4179.java
@@ -0,0 +1,19 @@
+package Day1205;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Boj_4179 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int y = Integer.parseInt(st.nextToken());
+		int x = Integer.parseInt(st.nextToken());
+		
+		for(int i=0;i<y;i++) {
+			
+		}
+	}
+}
