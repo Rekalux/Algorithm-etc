d3d2c95547bbe6f83a097f4a1272fcb016e7a89a
Rekalux <shycompany@naver.com>
2021-04-06 00:08:13 +0900

    Day0401_2

--- Only Algorithm/Year2021/Day0401/Boj_5624.py
+++ Only Algorithm/Year2021/Day0401/Boj_5624.py
@@ -7,13 +7,13 @@ two_set = set()
 three_set = set()
 
 for i in range(n):
-    if li[i] in three_set:
-        result += 1
+    for one in one_set:
+        if li[i] - one in two_set:
+            result += 1
+            break
     one_set.add(li[i])
     for one in one_set:
-        if -100000 <= one+li[i] <= 100000:
-            two_set.add(one+li[i])
-    for two in two_set:
-        if -100000 <= two+li[i] <= 100000:
-            three_set.add(two+li[i])
+        if -100000 <= one + li[i] <= 100000:
+            two_set.add(one + li[i])
+
 print(result)
