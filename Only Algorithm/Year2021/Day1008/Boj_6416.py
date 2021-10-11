f45b5f272ffbbb0681997cb4b1d1c3762d72655c
Rekalux <shycompany@naver.com>
2021-10-12 03:29:18 +0900

    Day1011

--- Only Algorithm/Year2021/Day1008/Boj_6416.py
+++ Only Algorithm/Year2021/Day1008/Boj_6416.py
@@ -3,14 +3,57 @@ import sys
 inp = sys.stdin.readlines()
 numbers = []
 for line in inp:
-    numbers.extend(map(int,line.split()))
-print(numbers)
+    numbers.extend(map(int, line.split()))
 link = {}
-for i in range(0,len(numbers),2):
-    if numbers[i] < 0 and numbers[i+1] < 0:
+node = {}  # 등장한 노드 : visit 여부
+down_list = set()
+case = 1
+find = True  # 트리라고 가정하고 시작함
+for i in range(0, len(numbers), 2):
+    up, down = numbers[i], numbers[i + 1]
+    if up < 0 and down < 0:
         break
-    if numbers[i] == 0 and numbers[i+1] == 0:
-
-        link = {}
+    if up == 0 and down == 0:
+        root_list = []
+        for key in node:
+            if not node[key]:
+                root_list.append(key)
+        if not node: # 들어온 노드가 없으면
+            print(f'Case {case} is a tree.')
+            case += 1
+            find = True
+            continue
+        if len(root_list) == 1:  # 루트노드가 단 하나인 경우만 인정
+            root = root_list[0]
+            stack = [root]  # 스택에 루트를 넣고 시작
+            while stack:
+                now_node = stack.pop()
+                for next_node in link[now_node]:
+                    if not node[next_node]:  # 가려는 노드가 이미 방문한 노드이면?
+                        find = False
+                        break
+                    stack.append(next_node)
+                    node[next_node] = False  # 방문했음을 처리
+            for key in node:
+                if node[key]:  # 방문 안 한 곳이 있다면?
+                    find = False
+        else:
+            find = False
+        print(f'Case {case} is a tree.' if find else f'Case {case} is not a tree.')
+        case += 1
+        find = True  # 트리라고 다시 초기화
+        link = {}  # 연산이 끝났으니 초기화
+        node = {}
+        down_list = set()
         continue
-    link[numbers[i]] = link.get(numbers[i],set())
+    if down not in link: # 자식 연결도 빈 링크를 넣어준다
+        link[down] = set()
+    if up not in link:  # 부모 연결에 자식 노드를 입력한다
+        link[up] = set()
+    link[up].add(down)
+    if up not in node:  # 새로운 노드면 추가
+        node[up] = False
+    node[down] = True  # 부모가 있는 노드는 True로 변화
+    if down in down_list: # 이미 노드를 한 번 받았으면
+        find = False # 두 개의 노드가 가르키고 있는 꼴이므로 트리가 아님 처리
+    down_list.add(down)
---
+++ Only Algorithm/Year2021/Day1010/Boj_11437.py
@@ -0,0 +1,46 @@
+import sys
+from collections import deque
+
+def maketree(root):  # bfs로 부모를 찾고 깊이를 저장하는 함수
+    q = deque([(root, -1)])  # 루트와 부모노드를 함께 저장
+    while q:
+        node, p = q.popleft()
+        for nextnode in link[node]:
+            if nextnode == p:
+                continue
+            table[nextnode].append(node)
+            depth[nextnode] = depth[node] + 1
+            q.append((nextnode, node))
+
+
+input = sys.stdin.readline
+n = int(input())
+link = [set() for _ in range(n)]
+table = [[] for _ in range(n)]
+depth = [0] * n
+for i in range(n - 1):  # input
+    a, b = map(lambda x: int(x) - 1, input().split())
+    link[a].add(b)
+    link[b].add(a)
+maketree(0)
+for d in range(1, 20):  # log(깊이)만큼 반복, log(100000) = 16.6xxx 이므로 넉넉히 20.
+    for i in range(1, n):  # 각 수의 2^d 번째 부모를 구하는 것으로 table을 채운다
+        if len(table[i]) < d or len(table[table[i][d - 1]]) < d:
+            continue
+        table[i].append(table[table[i][d - 1]][d - 1])
+m = int(input())
+for i in range(m):
+    a, b = map(lambda x: int(x) - 1, input().split())
+    if depth[a] != depth[b]:  # 두 노드의 깊이를 같게 만든다
+        a, b = max(a, b, key=lambda x: depth[x]), min(a, b, key=lambda x: depth[x])  # 깊이가 깊은 쪽을 a로 통일
+        while depth[a] != depth[b]:
+            for v in range(len(table[a]) - 1, -1, -1):
+                if depth[table[a][v]] >= depth[b]:
+                    a = table[a][v]
+                    break
+    while a != b and table[a][0] != table[b][0]:  # 두 수가 같거나 바로 다음 부모가 같을 때까지 반복
+        for v in range(len(table[a]) - 1, -1, -1):
+            if table[a][v] != table[b][v]:
+                a, b = table[a][v], table[b][v]
+                break
+    print(a + 1 if a == b else table[a][0] + 1)
