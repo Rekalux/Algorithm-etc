69918fc2a0f3b704cb8758f5152d35f66af54794
Rekalux <shycompany@naver.com>
2024-05-20 01:38:31 +0900

    Day0520

---
+++ Only Algorithm/Year2024/Day0520/Boj_25083.java
@@ -0,0 +1,23 @@
+package vin_algorithm;
+
+/*
+         ,r'"7
+r`-_   ,'  ,/
+ \. ". L_r'
+   `~\/
+      |
+      |
+
+출력하기
+*/
+
+public class Boj_25083 {
+    public static void main(String[] args) {
+        System.out.println("         ,r'\"7");
+        System.out.println("r`-_   ,'  ,/");
+        System.out.println(" \\. \". L_r'");
+        System.out.println("   `~\\/");
+        System.out.println("      |");
+        System.out.println("      |");
+    }
+}
---
+++ Only Algorithm/Year2024/Day0520/Boj_27866.java
@@ -0,0 +1,25 @@
+package vin_algorithm;
+
+import java.util.Scanner;
+
+public class Boj_27866 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String inputData = sc.nextLine();
+        System.out.println(inputData.charAt(sc.nextInt() - 1));
+
+        
+        int i = 0;
+        while(/*조건문*/){
+
+            /*반복 시킬 대상*/
+
+            i++;
+        }
+
+        for(int i = 0; /*조건문*/ ; i++){
+            /*반복 시킬 대상*/
+        }
+
+    }
+}
